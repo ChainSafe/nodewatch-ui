4a9f9b14983385bd00946c2fb36086cf54b40e3d
sweetpea22 <cindychau24@me.com>
2021-08-23 08:49:35 -0400

    Add license

---
+++ src/Components/Modules/Footer.tsx
@@ -0,0 +1,52 @@
+/*
+Copyright 2021 ChainSafe Systems
+SPDX-License-Identifier: LGPL-3.0-only
+*/
+
+import React from "react"
+import { createStyles, ITheme, makeStyles } from "@chainsafe/common-theme"
+import { Grid, Typography } from "@chainsafe/common-components"
+
+const useStyles = makeStyles(({ palette, constants, breakpoints }: ITheme) => {
+  return createStyles({
+    root: {
+      padding: `${constants.generalUnit * 3}px 0`,
+      position: "relative",
+      bottom: 0,
+      height: "1.5rem",
+      background: palette.background.paper,
+    },
+    bold: {
+      fontWeight: 600,
+    },
+    copyright: {
+      display: "flex",
+      alignItems: "center",
+      color: palette.additional["gray"][4],
+      [breakpoints.down("xl")]: {
+        marginLeft: constants.generalUnit * 2,
+      },
+      [breakpoints.up("xl")]: {
+        textAlign: "left",
+        fontSize: constants.generalUnit *2,
+      },
+    },
+  })
+})
+
+const Footer: React.FC = () => {
+  const currentYear = new Date().getFullYear()
+  const classes = useStyles()
+  return (
+    <footer className={classes.root}>
+      <Grid container>
+        <Grid item>
+          <Typography component="p" variant="body1" className={classes.copyright}>
+            &copy; {currentYear} ChainSafe Systems, All Rights Reserved.
+          </Typography>
+        </Grid>
+      </Grid>
+    </footer>
+  )
+}
+export default Footer
---
+++ src/Components/Modules/Navbar.tsx
@@ -0,0 +1,76 @@
+/*
+Copyright 2021 ChainSafe Systems
+SPDX-License-Identifier: LGPL-3.0-only
+*/
+
+import React from "react"
+import { createStyles, ITheme, makeStyles } from "@chainsafe/common-theme"
+import { Grid, Typography } from "@chainsafe/common-components"
+
+const useStyles = makeStyles(
+  ({ breakpoints, palette, zIndex, constants }: ITheme) => {
+    return createStyles({
+      container: {
+        width: "99%",
+        display: "flex",
+        justifyContent: "flex-start",
+        height: `${constants.headerHeight}px`,
+        position: "fixed",
+        top: 0,
+        padding: `${constants.generalUnit * 2}px`,
+        zIndex: zIndex?.layer4,
+        [breakpoints.down("sm")]: {
+          padding: "none",
+          width: "85%",
+        },
+        [breakpoints.up("xl")]: {
+          height: constants.generalUnit * 7,
+        },
+      },
+      navlink: {
+        color: palette.common.white.main,
+        textDecoration: "none",
+        fontWeight: "bold",
+        "&:hover": {
+          color: palette.primary.main,
+          transition: "ease-in 0.2s",
+        },
+        [breakpoints.up('sm')]: {
+          marginRight: `${constants.generalUnit * 2}px`
+        }
+      },
+
+    })
+  },
+)
+
+const NavBar: React.FC = () => {
+  const classes = useStyles()
+  return (
+    <div className={classes.container}>
+      <Grid container xs={12}>
+        <Grid item justifyContent="center" alignItems="flex-start">
+          <Typography variant="h5">
+            <a href="/" className={classes.navlink}>
+              Eth2 Nodewatch
+            </a>
+          </Typography>
+        </Grid>
+        <Grid item justifyContent="flex-end" alignItems="center" flexDirection="row">
+          <Typography variant="h5">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/ChainSafe/eth2-crawler-ui"
+              className={classes.navlink}
+            >
+              GitHub
+            </a>
+          </Typography>
+        </Grid>
+      </Grid>
+    </div>
+  )
+}
+
+export default NavBar
